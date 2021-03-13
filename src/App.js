467a5dde192e3693a2842553d2187d76c01e276d
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-03-13 15:44:12 +0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import logo from './logo.svg';
+import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase-config';
+import { useState } from 'react';
+
+firebase.initializeApp(firebaseConfig)
+
+function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSingIn = () => {
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName, photoURL, email} = res.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo : photoURL
+      }
+      setUser(signedInUser);
+
+      console.log(displayName, photoURL, email);
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.message);
+    })
+  }
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+    .then((res) =>{
+      const signedOutUser = {
+        isSignedIn: false,
+        name: '',
+        photo : '',
+        email: '',
+      }
+      setUser(signedOutUser);
+      console.log(res);
+    })
+    .catch((err) => {
+
+    })
+  }
+
+  return (
+    <div className="App">
+      {/* <button>Sign in</button> */}
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>sing out</button> :
+        <button onClick={handleSingIn}>sing in</button>
+      }
+      {
+        user.isSignedIn && <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your Email : {user.email}</p>
+          <img src={user.photo} alt=""/>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbkeZ0QxrK_fbAb2Fv0mylGpRa-M_PykA",
+    authDomain: "ema-jhon-simpol.firebaseapp.com",
+    projectId: "ema-jhon-simpol",
+    storageBucket: "ema-jhon-simpol.appspot.com",
+    messagingSenderId: "420267058645",
+    appId: "1:420267058645:web:54b5d6d6234f5bf092095c"
+  };
+
+  export default firebaseConfig;
